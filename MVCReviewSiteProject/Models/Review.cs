e63ee3d03e5d06926b5e0f20186ce000d55241da
ESchulstad <eschulstad@gmail.com>
2016-10-07 15:11:41 -0400

    Initial Commit

---
+++ MVCReviewSiteProject/Models/Review.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCReviewSiteProject.Models
+{
+    public class Review
+    {
+
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public string Content { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string PublisherName { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
